05f07a8b3fa844876efd3c8d523e71e10be33178
Guga Akopashvili <guga.akopashvili1711@gmail.com>
2025-01-28 12:03:03 +0400

    feature/footer
    adding footer

--- src/presenter/views/AboutUs.tsx
+++ src/presenter/views/AboutUs.tsx
@@ -1,9 +1,14 @@
 import Header from '../components/atoms/header/Header';
+import Footer from '../components/atoms/footer/Footer';
 
 export default function AboutUs() {
   return (
     <>
-      <Header />
+      <body className="flex min-h-screen flex-col">
+        <Header />
+        <div className="flex-1"></div>
+        <Footer />
+      </body>
     </>
   );
 }
