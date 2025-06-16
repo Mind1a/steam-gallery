56ffd40ee8504a55f71979bb1be4722764682fc2
AchiGel <ar.gelashvili@gmail.com>
2025-06-16 16:21:28 +0400

    relocate paginationButtons folder

--- src/presenter/components/atoms/pagination/Pagination.tsx
+++ src/presenter/components/atoms/pagination/Pagination.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import PaginationButtons from '../../../paginationButtons/PaginationButton';
+import PaginationButtons from '../paginationButtons/PaginationButton';
 
 interface PaginationProps {
   totalPages: number;
@@ -18,19 +18,19 @@ export default function Pagination({
   };
 
   return (
-    <div className="flex items-center justify-center gap-1">
+    <div className="flex justify-center items-center gap-1">
       <PaginationButtons
         text="Back"
         variant="left"
         onClick={() => handlePageChange(currentPage - 1)}
       />{' '}
-      <div className="flex w-full items-center justify-center gap-2">
+      <div className="flex justify-center items-center gap-2 w-full">
         {Array.from({ length: totalPages }, (_, i) => (
           <button
             className={`text-font-primary cursor-pointer rounded-lg bg-white px-4 py-2 shadow-[0px_4px_8px_0px_rgba(0,0,0,0.07)] ${
               currentPage === i + 1
                 ? 'text-primary-blue font-extrabold'
-                : 'font-normal text-font-primary'
+                : 'text-font-primary font-normal'
             }`}
             key={i}
             onClick={() => handlePageChange(i + 1)}
