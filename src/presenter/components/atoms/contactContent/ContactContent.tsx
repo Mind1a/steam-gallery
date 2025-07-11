31378204de07abe19e92b3b8a2991184ad4d77ea
AchiGel <ar.gelashvili@gmail.com>
2025-07-11 14:15:13 +0400

    fix/removed call

--- src/presenter/components/atoms/contactContent/ContactContent.tsx
+++ src/presenter/components/atoms/contactContent/ContactContent.tsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import {
   AboutUs,
   AboutUs2,
-  Call,
   Email,
   Location,
   FaceBook,
@@ -11,26 +10,26 @@ import {
 
 export const ContactContent = () => {
   return (
-    <div className="flex flex-1 flex-col">
-      <div className="slider-padding relative mx-auto w-full max-w-[1442px] overflow-hidden">
+    <div className="flex flex-col flex-1">
+      <div className="relative mx-auto w-full max-w-[1442px] overflow-hidden slider-padding">
         <img src={AboutUs} alt="About Us Background" />
       </div>
-      <div className="flex max-w-[1442px] flex-1 flex-col items-center justify-center gap-6 px-6 py-8 lg:px-12">
+      <div className="flex flex-col flex-1 justify-center items-center gap-6 px-6 lg:px-12 py-8 max-w-[1442px]">
         <div className="flex flex-col gap-4">
           <div className="flex gap-4">
-            <div className="flex min-h-[366px] flex-col justify-start gap-3">
-              <div className="text-font-primary mb-4 w-full text-left text-2xl font-light max-sm:mb-1">
+            <div className="flex flex-col justify-start gap-3 min-h-[366px]">
+              <div className="mb-4 max-sm:mb-1 w-full text-font-primary font-light text-2xl text-left">
                 კონტაქტი
               </div>
-              <div className="w-full flex-1 text-justify text-[16px] font-light text-[#333333] max-sm:mb-1">
+              <div className="flex-1 max-sm:mb-1 w-full font-light text-[#333333] text-[16px] text-justify">
                 გაქვთ შეკითხვები ან გჭირდებათ დახმარება? ჩვენ მზად ვართ
                 დაგეხმაროთ! დაგვიკავშირდით ნებისმიერ დროს ქვემოთ მოცემული
                 საკონტაქტო საშუალებებით:
               </div>
-              <div className="mx-auto w-full max-w-[400px] lg:hidden">
+              <div className="lg:hidden mx-auto w-full max-w-[400px]">
                 <img src={AboutUs2} alt="ჩვენს შესახებ" />
               </div>
-              <div className="flex w-full flex-col gap-8">
+              <div className="flex flex-col gap-8 w-full">
                 <div className="flex flex-col gap-4">
                   <Link
                     to="https://iliauni.edu.ge/en/studentebistvis/iliaunis-kiberlaboratoria-unilabi"
@@ -38,27 +37,18 @@ export const ContactContent = () => {
                     className="flex gap-4"
                   >
                     <img src={Location} alt="Location icon" />
-                    <p className="text-primary-blue text-[18px] font-semibold">
+                    <p className="font-semibold text-[18px] text-primary-blue">
                       თბილისი, ქაქუცა ჩოლოყაშვილის ქუჩა ⅗
                     </p>
                   </Link>
-                  <Link
-                    to="https://iliauni.edu.ge/en/studentebistvis/iliaunis-kiberlaboratoria-unilabi"
-                    target="_blank"
-                    className="flex gap-4"
-                  >
-                    <img src={Call} alt="Call icon" />
-                    <p className="text-primary-blue text-[18px] font-semibold">
-                      +9999999999999
-                    </p>
-                  </Link>
+
                   <Link
                     to="https://iliauni.edu.ge/en/studentebistvis/iliaunis-kiberlaboratoria-unilabi"
                     target="_blank"
                     className="flex gap-4"
                   >
                     <img src={Email} alt="Email icon" />
-                    <p className="text-primary-blue text-[18px] font-semibold">
+                    <p className="font-semibold text-[18px] text-primary-blue">
                       infounilab@iliauni.edu.ge
                     </p>
                   </Link>
@@ -72,7 +62,7 @@ export const ContactContent = () => {
                     <img
                       src={FaceBook}
                       alt="Facebook icon"
-                      className="hover:text-primary-red hover:fill-current"
+                      className="hover:fill-current hover:text-primary-red"
                     />
                   </Link>
                   <Link
@@ -85,11 +75,11 @@ export const ContactContent = () => {
                 </div>
               </div>
             </div>
-            <div className="hidden h-auto min-w-[432px] items-center justify-center lg:flex">
+            <div className="hidden lg:flex justify-center items-center min-w-[432px] h-auto">
               <img
                 src={AboutUs2}
                 alt="ჩვენს შესახებ"
-                className="h-[353px] w-[432px]"
+                className="w-[432px] h-[353px]"
               />
             </div>
           </div>
